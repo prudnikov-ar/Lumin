cab069dc8dc30fa032ebc3866a1711622888aaca
prudnikov_ar <prudnikov_ar@icloud.com>
2025-06-27 20:46:22 +0700

    Initial Commit

---
+++ Lumin/LuminApp.swift
@@ -0,0 +1,17 @@
+//
+//  LuminApp.swift
+//  Lumin
+//
+//  Created by Андрей Прудников on 27.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct LuminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
